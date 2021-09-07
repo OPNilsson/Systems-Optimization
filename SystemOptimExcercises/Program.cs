f0d73dcaa8495b1ba74c575222fbb2c083efcbef
Mads Finnerup <Finnerup1@gmail.com>
2021-09-07 11:47:24 +0200

    initial push

---
+++ SystemOptimExcercises/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SystemOptimExcercises
+{
+    class MainClassss
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Dictionary<int, string> test = new Dictionary<int,string>();
+
+
+            
+        }
+    }
+}
