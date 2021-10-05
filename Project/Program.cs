f8bc8cc76dacb698890681116db3ebc0510e99a5
yre <s202483@student.dtu.dk>
2021-10-05 11:51:09 +0200

    First commit

---
+++ Project/Program.cs
@@ -0,0 +1,14 @@
+﻿using Google.OrTools.Sat;
+using System;
+
+namespace Project
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CpModel model = new CpModel();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- README.md
+++
@@ -1,6 +0,0 @@
-# Systems Optimization Project
-
-This project was developed in a group of 4 for System Optimizations (02229) course at DTU.
-
-## Objective
-The objective of the project is to create a task scheduler with network capabilities.
